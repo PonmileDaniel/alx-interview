634cac38288f99642957364ddeb24bb06a704e4f
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-06 17:40:10 +0000

    Pascal tiangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+"""
+    Pascal triangle
+"""
+
+
 def pascal_triangle(n):
+
+    """Create a func for pascal_triangle"""
     if n <= 0:
         return []
     tri = [[1]]
 
     for i in range(1, n):
-        row = [1]
+        row = tri[-1]
+        current_row = [1]
         for j in range(1, i):
-            row.append(tri[i - 1][j - 1] + tri[i - 1][j])
-        row.append(1)
-        tri.append(row)
+            current_row.append(row[j - 1] + row[j])
+        current_row.append(1)
+        tri.append(current_row)
     return tri
