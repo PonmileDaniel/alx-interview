aa6f98d92b3bc31fa2ae81a5da98ae67d8686a7c
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-06 17:19:55 +0000

    py code style

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,10 +4,10 @@ def pascal_triangle(n):
         return []
     tri = [[1]]
 
-    for i in range (1, n):
+    for i in range(1, n):
         row = [1]
         for j in range(1, i):
-            row.append(tri[i - 1][j - 1] + tri[i -1][j])
+            row.append(tri[i - 1][j - 1] + tri[i - 1][j])
         row.append(1)
         tri.append(row)
     return tri
