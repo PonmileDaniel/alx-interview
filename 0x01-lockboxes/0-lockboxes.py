1fb28c4f14cbf613efb181136e2b6780e8628cd5
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-13 22:12:38 +0000

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+    Annontation for Variable
+    """
+
+
+def canUnlockAll(boxes):
+    """Total number of boxes"""
+    n = len(boxes)
+    visited = set()
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+
+        if current_box not in visited:
+            visited.add(current_box)
+            for key in boxes[current_box]:
+                if key < n and key not in visited:
+                    stack.append(key)
+
+    return len(visited) == n
