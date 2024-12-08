8ebecd6b682ca046ef58ccfc1b4afc8c712e211c
root <root@DESKTOP-ODC38F9.localdomain>
2024-12-09 00:35:30 +0100

    push

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,6 +3,7 @@
 Calculate the perimeter of an island in a grid.
 """
 
+
 def island_perimeter(grid):
     """
     Returns the perimeter of the island described in the grid.
@@ -22,19 +23,19 @@ def island_perimeter(grid):
             if grid[row][col] == 1:
                 # Add 4 for the current cell
                 perimeter += 4
-                
+
                 # Check the above cell
                 if row > 0 and grid[row-1][col] == 1:
                     perimeter -= 1
-                
+
                 # Check the below cell
                 if row < rows - 1 and grid[row+1][col] == 1:
                     perimeter -= 1
-                
+
                 # Check the left cell
                 if col > 0 and grid[row][col-1] == 1:
                     perimeter -= 1
-                
+
                 # Check the right cell
                 if col < cols - 1 and grid[row][col+1] == 1:
                     perimeter -= 1
